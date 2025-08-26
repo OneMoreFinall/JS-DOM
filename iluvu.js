a09f36276d2aa598eb571b990bd0f72a7d99c6d0
OneMoreFinall <zakihakim385@gmail.com>
2025-08-26 13:18:41 +0700

    error kelar

--- iluvu.js
+++ iluvu.js
@@ -1,36 +1,27 @@
-const input = document.getElementById("text");
-const button = document.getElementById("submit");
-const list = document.getElementById("list");
+const input = document.getElementById("text")
+const button = document.getElementById("submit")
+const list = document.getElementById("list")
 let itemEdit = null;
 
 button.addEventListener("click", function() {
-    if (input.value.trim() !== "") {
-        const li = document.createElement("li")
-        li.textContent = input.value
-        list.appendChild(li)
-        input.value = ""
-    } else {
-        alert("MANA TASKNYA")
-    }
-})
-
-
     //variabel untuk nyimpan input value
-    const teksv = input.value.trim();
+    const teksv = input.value
+    
     //validasi input
     if (teksv !== "") {
          if (itemEdit) {
                 const span = itemEdit.querySelector("span");
                 span.textContent = teksv;
                 itemEdit = null
-                button.classList.remove("update-button");
-                button.value = "Add To Do List";
+                button.style.color = "";
+                button.style.backgroundColor = "";
+                button.value = "Add List";
          } else {
                 //membuat elemen li 
                 const li = document.createElement("li")
                 //text item
                 const span = document.createElement("span")
-                span.textContent = teksv
+                span.textContent = input.value
                 li.appendChild(span)
 
 
@@ -41,7 +32,6 @@ button.addEventListener("click", function() {
                 editBtn.addEventListener("click", function(){
                     input.value = span.textContent
                     button.value = "Update List"
-                    button.classList.add("update-button");
                     itemEdit = li
         })
         li.appendChild(editBtn)
@@ -59,7 +49,8 @@ button.addEventListener("click", function() {
             input.value = ""
     }
 
-    //peringatan kalau input kosong
-    else{
-        alert("MANA TASKNYA")
-    }
+        //peringatan kalau input kosong
+        else{
+            alert("MANA TASKNYA")
+        }})
+
