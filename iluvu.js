3d85c8dd0481119491b136f172d55c3c21202100
OneMoreFinall <zakihakim385@gmail.com>
2025-08-26 13:17:07 +0700

    finishing selesai

--- iluvu.js
+++ iluvu.js
@@ -1,6 +1,10 @@
 const input = document.getElementById("text")
 const button = document.getElementById("submit")
 const list = document.getElementById("list")
+const input = document.getElementById("text");
+const button = document.getElementById("submit");
+const list = document.getElementById("list");
+let itemEdit = null;
 
 button.addEventListener("click", function() {
     if (input.value.trim() !== "") {
@@ -13,3 +17,52 @@ button.addEventListener("click", function() {
     }
 })
 
+
+    //variabel untuk nyimpan input value
+    const teksv = input.value.trim();
+    //validasi input
+    if (teksv !== "") {
+         if (itemEdit) {
+                const span = itemEdit.querySelector("span");
+                span.textContent = teksv;
+                itemEdit = null
+                button.classList.remove("update-button");
+                button.value = "Add To Do List";
+         } else {
+                //membuat elemen li 
+                const li = document.createElement("li")
+                //text item
+                const span = document.createElement("span")
+                span.textContent = teksv
+                li.appendChild(span)
+
+
+                //bikin tombol edit 
+                const editBtn = document.createElement("button")
+                editBtn.textContent = "Edit"
+                editBtn.className = "edit"
+                editBtn.addEventListener("click", function(){
+                    input.value = span.textContent
+                    button.value = "Update List"
+                    button.classList.add("update-button");
+                    itemEdit = li
+        })
+        li.appendChild(editBtn)
+
+                //bikin tombol delete
+                const deleteBtn = document.createElement("button")
+                deleteBtn.textContent = "Delete"
+                deleteBtn.className = "delete"
+                deleteBtn.addEventListener("click", function(){
+                    li.remove()
+        })
+        li.appendChild(deleteBtn)
+            list.appendChild(li);
+    }
+            input.value = ""
+    }
+
+        //peringatan kalau input kosong
+        else{
+            alert("MANA TASKNYA")
+        }})
